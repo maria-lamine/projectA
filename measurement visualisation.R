5b056669fe8a3393968fee5e2f82b641a45bbfe7
maria-lamine <maria.lamine.18@edu.uiz.ac.ma>
2026-07-20 14:40:34 +0200

    Delete measurement visualisation.R

--- measurement visualisation.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-
-ggplot(combined_data, aes(x = group, y = measurement, fill = group)) +
-  geom_boxplot() + 
-  geom_jitter(width = 0.2) + # Shows individual data points
-  theme_minimal()
